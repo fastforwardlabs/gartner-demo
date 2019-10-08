17ff12912b8b02ab15fa1ca98dd19f9267dd910e
Michael Gregory <mgregory@cloudera.com>
2019-10-08 12:58:01 +0000

    fixed 5 and 6

--- 5_model_deployment_train.py
+++ 5_model_deployment_train.py
@@ -9,6 +9,7 @@ import os, shutil
 if ( not os.path.exists('/etc/hadoop/conf/hive-site.xml')):
   shutil.copyfile("/home/cdsw/utils/hive-site.xml", "/etc/hadoop/conf/hive-site.xml")
 
+import cdsw
 from pyspark.sql import SparkSession
 from sklearn.model_selection import train_test_split
 import pandas as pd
@@ -16,7 +17,7 @@ import numpy as np
 
 spark = SparkSession\
     .builder\
-    .appName("Airline TPOT")\
+    .appName("Airline sklearn")\
     .config("spark.executor.memory", "16g")\
     .config("spark.executor.cores","4")\
     .config("spark.driver.memory","6g")\
@@ -29,12 +30,28 @@ spark.sql("USE default")
 spark.sql("SHOW tables").show()
 spark.sql("DESCRIBE flights").show()
 
+model_path = '/home/cdsw/models/'
+model_name = 'lr_model.pkl'
+enc_name = 'lr_enc.pkl'
+
 # Read the data into Spark
 #flight_df=spark.read.parquet("s3a://ml-field/demo/flight-analysis/data/airline_parquet_2/",)
- 
+
+#try data set_size via Experiments
+if len (sys.argv) == 2:
+  if sys.argv[1].split(sep='=')[0]=='set_size' and isinstance(float(sys.argv[1].split(sep='=')[1]), float):
+    set_size = float(sys.argv[1].split(sep='=')[1])
+  else:
+    sys.exit("Invalid Arguments passed to Experiment")
+else:
+    set_size = 5000
+
+cdsw.track_metric("Set Size", set_size)
+
+
 # Pull a sample of the dataset into an in-memory
 # Pandas dataframe. Use a smaller dataset for a quick demo.
-flight_df_local = spark.sql("SELECT * FROM `default`.`flights`").limit(5000).toPandas()
+flight_df_local = spark.sql("SELECT * FROM `default`.`flights`").limit(set_size).toPandas()
 
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LogisticRegression
@@ -50,6 +67,15 @@ X_transform = enc.fit_transform(X_onehot)
 
 reg = LogisticRegression().fit(np.hstack([X_transform, X_num]), y)
 
+accuracy = reg.score(np.hstack([X_transform, X_num]), y)
+cdsw.track_metric("Accuracy", accuracy)
+
+
 import joblib
-joblib.dump(reg, "xx_model_deployment_model.pkl")
-joblib.dump(enc, "xx_model_deployment_encoding.pkl")
+with open(model_path+model_name, 'wb') as fid:
+    joblib.dump(reg, fid)
+cdsw.track_file(model_path+model_name)
+
+with open(model_path+enc_name, 'wb') as fid:
+    joblib.dump(enc, fid)
+cdsw.track_file(model_path+enc_name)
--- 6_model_deployment_predict.py
+++ 6_model_deployment_predict.py
@@ -3,8 +3,18 @@ import joblib
 import pandas as pd
 import numpy as np
 
-enc = joblib.load("xx_model_deployment_encoding.pkl")
-model = joblib.load("xx_model_deployment_model.pkl")
+model_path = '/home/cdsw/models/'
+model_name = 'lr_model.pkl'
+enc_name = 'lr_enc.pkl'
+
+import os, shutil
+if ( os.path.exists('/home/cdsw/'+enc_name)):
+  shutil.move('/home/cdsw/'+enc_name, model_path+enc_name)
+enc = joblib.load(model_path+enc_name)
+
+if ( os.path.exists('/home/cdsw/'+model_name)):
+  shutil.move('/home/cdsw/'+model_name, model_path+model_name)
+model = joblib.load(model_path+model_name)
 
 def predict(ipt):
   ipt_df = pd.DataFrame([ipt])
@@ -17,12 +27,5 @@ def predict(ipt):
   return {"delay_probability": model.predict_proba(ipt_final)[0,1]}
     
 #test
-#ipt = {
-#  "CRSDepTime": 630,
-#  "Dest": "SJC",
-#  "Distance": 308,
-#  "Origin": "LAX",
-#  "UniqueCarrier": "PS"
-#}
-#
+#ipt = {"CRSDepTime": 630, "Dest": "SJC", "Distance": 308, "Origin": "LAX", "UniqueCarrier": "PS"}
 #predict(ipt)
